58b68907efe743bb5ef2192908b7e18d52bc9393
root <root@localhost.localdomain>
2024-08-08 02:44:45 -0700

    connection_pool

---
+++ build/src/CMakeFiles/connpool.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/connpool.dir/CommonConnectionPool.cpp.o"
+  "CMakeFiles/connpool.dir/Connection.cpp.o"
+  "CMakeFiles/connpool.dir/main.cpp.o"
+  "../../bin/connpool.pdb"
+  "../../bin/connpool"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/connpool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
